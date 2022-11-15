99c44488b149ef50d26586f32275b2dc1e170956
wendell <coder.wendell@gmail.com>
2022-11-16 00:35:42 +0800

    状态注释

--- src/main/java/com/fgo/management/enums/OrderStatus.java
+++ src/main/java/com/fgo/management/enums/OrderStatus.java
@@ -2,26 +2,52 @@ package com.fgo.management.enums;
 
 public enum OrderStatus {
 
+    /**
+     * 刚创建的订单
+     */
     INIT,
 
+    /**
+     * 代练中
+     */
     RUNNING,
 
+    /**
+     * 代练停止
+     */
     STOPPED,
 
+    /**
+     * 已完成
+     */
     FINISHED,
-
+    /**
+     * 未完成
+     */
     UNFINISHED,
-
+    /**
+     * 活动结束未完成
+     */
     UNFINISHED_EVENT_DONE,
-
+    /**
+     * 异常
+     */
     ABNORMAL,
-
+    /**
+     * 密码异常
+     */
     PASSWORD_EXCEPTION,
-
+    /**
+     * 体力不足
+     */
     OUT_OF_ACTIVE_POWER,
-
+    /**
+     * 黑名单
+     */
     BLACK_LIST,
-
+    /**
+     * 已结算
+     */
     SETTLED,
 
 
