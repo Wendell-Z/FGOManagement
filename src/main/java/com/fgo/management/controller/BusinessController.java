9fc0fd9f23bd77009561563bb4fda4018a6003c4
wendell <coder.wendell@gmail.com>
2022-11-18 21:38:33 +0800

    业务执行顺序查询接口

--- src/main/java/com/fgo/management/controller/BusinessController.java
+++ src/main/java/com/fgo/management/controller/BusinessController.java
@@ -1,5 +1,6 @@
 package com.fgo.management.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.fgo.management.annotations.LoginValid;
 import com.fgo.management.dto.MyResponse;
 import com.fgo.management.model.BusinessOrder;
@@ -11,7 +12,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/business")
 @RestController
@@ -41,5 +44,15 @@ public class BusinessController {
     public MyResponse progress(HttpServletRequest request, @RequestParam long orderId) {
         // 订单服务
         return MyResponse.success(orderDetailService.progress(orderId));
+    }
+
+    @GetMapping("/order")
+    @LoginValid
+    public MyResponse businessOrder() {
+        ParamConfig paramConfig = paramConfigService.queryByParam("BUSINESS", "ORDER");
+        return MyResponse.success(JSONUtil.toList(paramConfig.getParamValue(), BusinessOrder.class)
+                .stream()
+                .sorted(Comparator.comparing(BusinessOrder::getOrder))
+                .collect(Collectors.toList()));
     }
 }
