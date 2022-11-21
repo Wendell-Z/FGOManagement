478f5590ba1e6266520d490cfa9d2e6f9ffa3c7c
wendell <coder.wendell@gmail.com>
2022-11-21 23:28:18 +0800

    取消json切面

--- src/main/java/com/fgo/management/aspects/OrderDetailToJsonAspect.java
+++ src/main/java/com/fgo/management/aspects/OrderDetailToJsonAspect.java
@@ -11,11 +11,10 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 
-@Component
+//@Component
 @Aspect
 public class OrderDetailToJsonAspect {
 
